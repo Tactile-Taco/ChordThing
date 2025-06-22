b4d21fa9eee5e5adb153a89dae1d648cd900958f
Luke Hill <52799402+Luke-Hill-Tactical-Taco@users.noreply.github.com>
2025-06-22 14:01:39 -0400

    small changes idk

--- src/main.js
+++ src/main.js
@@ -8,36 +8,40 @@ function get_text() {
   return text;
 }
 
-window.addEventListener("keydown", function(e) {
-  if (e.code === "Space" && e.target === document.body) {
-    e.preventDefault();
-  }
-});
-
-var typer = document.getElementById("typer");
-function firstLetter(node) {
-  if (!node) throw "Non-letter leaf descendant reached";
-  if (typer != node && !typer.contains(node)) throw "Must search within typer element";
-
-  return (node.className == "letter")? node : firstLetter(node.firstChild);
+function split_chords(s) {
+  //TODO: find all chords in s:String and return split. You might need to add another param for chord list or make it global...
+  return [s];
+}
+function wrap_text(s) {
+  //TODO:
+  // return s;nd all chords in s:String and return spli. You might need to add another param for chord list or make it global...t
+  return s; 
 }
 
-function findNextLetter(letter) {
+var typer = document.getElementById("typer");
+function firstCharacter(node) {
+  if (!node) throw "Non-character leaf descendant reached";
+  if (typer != node && !typer.contains(node)) throw "Must search within typer element";
+
+  return (node.className == "character")? node : firstCharacter(node.firstChild);
+}
+
+function findNextCharacter(character) {
   do {
-    letter = letter.nextElementSibling;
-    if (letter) return firstLetter(letter);
+    character = character.nextElementSibling;
+    if (character) return firstCharacter(character);
     
-    letter = letter.parentElement;
-  } while (letter != typer);
+    character = character.parentElement;
+  } while (character != typer);
 }
 
 function selectCursor() {
   window.getSelection().setPosition(document.getElementById("cursor").firstChild, 0);
 }
 
-function cursorNextLetter() {
+function cursorNextCharacter() {
   const cursor = document.getElementById("cursor");
-  let next = findNextLetter(cursor);
+  let next = findNextCharacter(cursor);
   cursor.removeAttribute('id');
   next.id = "cursor";
   window.getSelection.setPosition(next.firstChild, 0);
@@ -46,10 +50,19 @@ function cursorNextLetter() {
 typer.addEventListener("mousedown", function (e) {
   e.preventDefault();
   e.stopPropagation();
+  selectCursor();
 }, { capture: false })
 
 typer.addEventListener("focus", function(e) {
-  selectNextLetter();
+  selectCursor();
 })
+
+window.addEventListener("keydown", function(e) {
+  if (e.code === "Space" && e.target === document.body) {
+    e.preventDefault();
+  } else if (typer.contains(document.activeElement())) {
+    cursorNextCharacter();
+  }
+});
 
 typeDisplay.append(get_text());
